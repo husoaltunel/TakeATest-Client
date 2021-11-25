ed5d58130079c16006b6f19f452a7fa3a7e0e479
Hüseyin ALTUNEL <65026269+husoaltunel@users.noreply.github.com>
2021-11-25 03:54:16 +0300

    added components

--- src/app/admin/create-exam/create-exam.component.ts
+++ src/app/admin/create-exam/create-exam.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { optionLetters } from 'src/app/constants/option-letters';
 import { ExamModel } from 'src/app/models/exam.model';
+import { OptionModel } from 'src/app/models/option.model';
+import { QuestionModel } from 'src/app/models/question.model';
 
 
 @Component({
@@ -9,19 +11,16 @@ import { ExamModel } from 'src/app/models/exam.model';
   styleUrls: ['./create-exam.component.css']
 })
 export class CreateExamComponent implements OnInit {
-
   exam: ExamModel;
   questionQuantity: number;
   optionQuantity: number;
-  optionLetters : string[];
+  optionLetters: string[];
   constructor() {
     this.exam = new ExamModel();
-    this.exam.questions = [];
-    this.exam.questions.forEach(question => question.options = []);
     this.questionQuantity = 4;
     this.optionQuantity = 4;
     this.optionLetters = optionLetters;
-    console.log(this.exam)
+    this.createNullExam();
   }
 
   ngOnInit(): void {
@@ -30,7 +29,19 @@ export class CreateExamComponent implements OnInit {
 
   counter(i: number) {
     return new Array(i);
-}
+  }
+
+  createNullExam(){
+    for (let index = 0; index < this.questionQuantity; index++) {
+       this.exam.questions[index] = new QuestionModel();
+      for (let j = 0; j < this.optionQuantity; j++) {
+        this.exam.questions[index].options[j] = new OptionModel();        
+      }
+    }
+  }
+  show(){
+    console.log(this.exam)
+  }
 
 
 }
